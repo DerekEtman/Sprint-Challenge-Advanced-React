fcf77f3a23ac04b98ea16f440eb0069e13eed56b
Derek Etman <Dereketman@gmail.com>
2019-09-06 10:43:22 -0700

    Got 2 unit tests thanks to Dustin Myers.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import Display from './components/Display';
+import PlayerCard from './components/PlayerCard'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// This 
+test('should have Name Alex Morgan',  async ()=>{
+  const {findByText} = rtl.render(<App />)
+  findByText(/alex morgan/i)
 });
+
+test('should have Country of United States',  async ()=>{
+  const {findByText} = rtl.render(<App />)
+  findByText(/united states/i)
+})
